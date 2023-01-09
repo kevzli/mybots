b74eafb1bf624005d5ff0618de2dd66fefa074a6
Kevin Li <71240367+kevzli@users.noreply.github.com>
2023-01-09 14:50:03 -0600

    update
    update

--- simulate.py
+++ simulate.py
@@ -1,1 +1,9 @@
-pass
+import pybullet as p
+import time
+physicsClient = p.connect(p.GUI)
+p.loadSDF("box.sdf")
+for i in range(1, 1001):
+    p.stepSimulation()
+    time.sleep(0.0166666667)
+    print(i)
+p.disconnect()
